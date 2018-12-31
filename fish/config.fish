337a1be12e048d7efd9d2708769dca2ba6357312
tom <tom@praschan.de>
2018-12-31 17:14:39 +0100

    shortcut for editing in vim

--- fish/config.fish
+++ fish/config.fish
@@ -14,7 +14,7 @@ end
 source $DOTFILES/fish/.fishalias
 
 #fish_vi_key_bindings
-set -U fish_key_bindings fish_vi_key_bindings
+set -U fish_key_bindings fish_vi_key_bindings #alt-e or alt-v to edit in vim
 function fish_mode_prompt ; end
 
 #Theme: omf install shellder
