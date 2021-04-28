09ad6d65621c7b9805882d4c2181ae6fe21b64d1
tom-anders <13141438+tom-anders@users.noreply.github.com>
2021-04-28 01:14:11 -0700

    Add treesitter function jumps

--- vim/lua/treesitterConfig.lua
+++ vim/lua/treesitterConfig.lua
@@ -16,5 +16,21 @@ require'nvim-treesitter.configs'.setup {
         ["ia"] = "@parameter.inner",
       },
     },
+    move = {
+        enable = true,
+        set_jumps = true,
+        goto_next_start = {
+            ["}f"] = "@function.outer",
+        },
+        goto_next_end = {
+            ["}F"] = "@function.outer",
+        },
+        goto_previous_start = {
+            ["{f"] = "@function.outer",
+        },
+        goto_previous_end = {
+            ["{F"] = "@function.outer",
+        },
+    }
   },
 }
